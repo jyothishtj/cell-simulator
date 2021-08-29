d960eb87de50d3a2b45d10d26135335f584112a1
Jyothish tj <jyothish.pillai@educationhorizons.com.au>
2021-08-29 15:11:54 +1000

    updated the run game function for the next generation

--- src/Components/GameOfLife.tsx
+++ src/Components/GameOfLife.tsx
@@ -1,4 +1,4 @@
-import {FC, useState} from "react";
+import {FC, useState, useCallback} from "react";
 import styles from "./GameOfLife.module.css";
 
 const generateInitialCells = (): number[][] => {
@@ -10,11 +10,48 @@ const generateInitialCells = (): number[][] => {
   return rows;
 };
 
+const movements = [
+  [0, 1], // right
+  [0, -1], // left
+  [1, -1], // top left
+  [-1, 1], // top right
+  [1, 1], // top
+  [-1, -1], // bottom
+  [1, 0], // bottom right
+  [-1, 0], // bottom left
+];
+
 export const GameOfLife: FC = () => {
   const [cell, setCell] = useState(() => {
     return generateInitialCells(); // On first render none of the cells are selected
   });
+  const runGame = useCallback(() => {
+    let cellCopy = JSON.parse(JSON.stringify(cell));
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        let neighbours = 0;
 
+        movements.forEach(([x, y]) => {
+          const newX = i + x;
+          const newY = j + y;
+
+          if (newX >= 0 && newX < 10 && newY >= 0 && newY < 10) {
+            // Identify the neighbours
+            neighbours += cell[newX][newY];
+          }
+        });
+
+        if (neighbours < 2 || neighbours > 3) {
+          // If the neighbours is less than 2 or greater than 3 it dies
+          cellCopy[i][j] = 0;
+        } else if (cell[i][j] === 0 && neighbours === 3) {
+          cellCopy[i][j] = 1;
+        }
+      }
+    }
+
+    setCell(cellCopy);
+  }, [cell]);
   return (
     <div>
       <h1 className={styles.heading}>Game of Life</h1>
@@ -41,7 +78,7 @@ export const GameOfLife: FC = () => {
       </div>
 
       <div className={styles.gameButtons}>
-        <button onClick={() => alert("Calculate next generation")}>Next generation</button>
+        <button onClick={() => runGame()}>Next generation</button>
         <button onClick={() => setCell(() => generateInitialCells())}>Reset</button>
       </div>
     </div>
